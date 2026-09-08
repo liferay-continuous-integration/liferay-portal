51c997e375f4fc849df7c2f902721b6be088ac8b
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-09-08 16:57:58 +0200

    LPD-50101 buildREST

---
+++ modules/util/portal-tools-rest-builder-test-client-js/src/models/OneOfPropertyTestEntity.ts
@@ -0,0 +1,41 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+
+/**
+ * @author Alejandro Tardín
+ * @generated
+ */
+
+	/**
+	* Test Component to test the JSON serialization of a property declared with oneOf.
+	*/
+	export class OneOfPropertyTestEntity {
+			"name"?: string;
+			"oneOfProperty"?: any;
+
+		static "discriminator": string | undefined = undefined;
+
+	static "attributeTypeMap": Array<{
+		baseName: string;
+		name: string;
+		type: string;
+	}> = [
+		{
+			baseName: "name",
+			name: "name",
+			type: "string",
+		},
+		{
+			baseName: "oneOfProperty",
+			name: "oneOfProperty",
+			type: "any",
+		},
+		];
+
+		static getAttributeTypeMap() {
+				return OneOfPropertyTestEntity.attributeTypeMap;
+		}
+	}
